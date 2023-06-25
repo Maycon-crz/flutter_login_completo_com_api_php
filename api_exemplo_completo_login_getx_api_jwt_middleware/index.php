0bb0666fee1f2f593bb159ba831e3c1b248fa281
Maycon Nascimento de Oliveira <63969109+Maycon-crz@users.noreply.github.com>
2023-06-24 23:33:56 -0300

    inicionado cadastro de usuário

--- api_exemplo_completo_login_getx_api_jwt_middleware/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-	header('Content-Type: application/json; charset=UTF-8');
-	header("Access-Control-Allow-Origin: *");
-	require_once "src/rest.php";	
-	
-	if(isset($_SERVER['REQUEST_URI']) && !empty($_SERVER['REQUEST_URI'])){			
-		$rest = new Rest();
-		echo $rest->run($_SERVER['REQUEST_URI']);		
-	}
