b2a0e45f8a9e63e77182a7aa07c5101e0f318d3e
Maycon Nascimento de Oliveira <63969109+Maycon-crz@users.noreply.github.com>
2023-06-27 15:43:24 -0300

    Revert "Back-end e Front-end Cadastro de usuário"
    This reverts commit 2b63fe5598d925645dfeb70d57a255b12acc1817.

--- login_flutter/lib/app/modules/bindings/user_registration_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:login_flutter/app/modules/controllers/user_registration_controller.dart';
-
-class UserRegistrationBinding implements Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut(() => UserRegistrationController());
-  }
-}
--- login_flutter/lib/app/modules/views/login_page.dart
+++ login_flutter/lib/app/modules/views/login_page.dart
@@ -5,7 +5,6 @@ import 'package:login_flutter/app/modules/utils/default_colors.dart';
 import 'package:login_flutter/app/modules/views/components/app_bar_component.dart';
 import 'package:login_flutter/app/modules/views/components/elevated_buttom_component.dart';
 import 'package:login_flutter/app/modules/views/components/text_form_field_component.dart';
-import 'package:login_flutter/app/routes/route_name.dart';
 
 class LoginPage extends GetView<LoginController> {
   const LoginPage({Key? key}) : super(key: key);
@@ -28,8 +27,6 @@ class LoginPage extends GetView<LoginController> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Icon(Icons.account_circle, size: 230),
-                const SizedBox(height: 30),
                 textFormFieldComponent(
                   context: context,
                   controller: controller.emailController.value,
@@ -55,10 +52,7 @@ class LoginPage extends GetView<LoginController> {
                 const SizedBox(height: 10),
                 elevatedButtomComponent(
                   title: "Cadastre-se",
-                  function: () {
-                    // print("Boora");
-                    Get.offNamed(RouteName.userRegistration);
-                  },
+                  function: () {},
                 ),
               ],
             ),
